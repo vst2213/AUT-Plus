3317265e42df7a1114783f5cda0c3391b03d5e45
vst2213 <vst2213@autuni.ac.nz>
2024-08-22 18:57:08 +1200

    Update Main.js

--- Main.js
+++ Main.js
@@ -8,17 +8,3 @@ console.log("Hello, World!");
 function greetUser(name) {
     console.log("Hello, " + name + "! Welcome to the world of JavaScript.");
 }
-
-// Define a user's name
-let userName = "Alice";
-
-// Call the greetUser function with the user's name
-greetUser(userName);
-
-// Example of a simple arithmetic operation
-let a = 7;
-let b = 3;
-let sum = a + b;
-
-// Display the result of the arithmetic operation
-console.log("The sum of " + a + " and " + b + " is: " + sum);
