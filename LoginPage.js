43568f7c9e84314bd23c6b4dfe3488a2a13ffa95
Dongwonlee8 <134839946+Dongwonlee8@users.noreply.github.com>
2024-09-19 22:32:13 +1200

    LoginPage.js

---
+++ LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Login Page</h1>
+      <p>Please log in to continue.</p>
+    </div>
+  );
+};
+
+export default LoginPage;
